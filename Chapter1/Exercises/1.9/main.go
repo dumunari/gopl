64e7f2917f1b6e923df45794bf6654abd7260b3b
Eduardo Munari <munari.edu@gmail.com>
2023-06-18 20:15:07 -0300

    add exercises 1.7, 1.8 and 1.9

---
+++ Chapter1/Exercises/1.9/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		addProtocolIfNeeded(&url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("\nStatus code: %d\n", resp.StatusCode)
+	}
+}
+
+func addProtocolIfNeeded(url *string) {
+	if !strings.HasPrefix(*url, "http://") && !strings.HasPrefix(*url, "https://") {
+		*url = "http://" + *url
+	}
+}
